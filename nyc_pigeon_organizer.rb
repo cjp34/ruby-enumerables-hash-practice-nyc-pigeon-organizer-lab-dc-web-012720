92d8736bf1f17e5da03abc026ec5f97a61aea758
Carl Parm <carlparm@gmail.com>
2020-01-24 19:20:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  data.each do |color_gender_lives, value|
+    value.each do |facts, all_names|
+      all_names.each do |name|
+        if new_pigeon_hash[name] == nil
+          new_pigeon_hash[name] = {}
+        end
+        if new_pigeon_hash[name][color_gender_lives] == nil
+          new_pigeon_hash[name][color_gender_lives] = []
+        end
+      new_pigeon_hash[name][color_gender_lives].push(facts.to_s)
+  end
+end
+end
+new_pigeon_hash
 end
